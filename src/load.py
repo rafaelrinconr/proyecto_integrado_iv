9bd1bd17b1c567305ea0afb0948d111eede5b84c
rafaelrinconr <rafael.rincon@est.iudigital.edu.co>
2025-02-25 23:26:13 -0500

    Proyecto sin modificaciones

---
+++ src/load.py
@@ -0,0 +1,18 @@
+from typing import Dict
+
+from pandas import DataFrame
+from sqlalchemy.engine.base import Engine
+
+
+def load(data_frames: Dict[str, DataFrame], database: Engine):
+    """Load the dataframes into the sqlite database.
+
+    Args:
+        data_frames (Dict[str, DataFrame]): A dictionary with keys as the table names
+        and values as the dataframes.
+    """
+    # TODO: Implementa esta función. Por cada DataFrame en el diccionario, debes
+    # usar pandas.DataFrame.to_sql() para cargar el DataFrame en la base de datos
+    # como una tabla.
+    # Para el nombre de la tabla, utiliza las claves del diccionario `data_frames`.
+    raise NotImplementedError
